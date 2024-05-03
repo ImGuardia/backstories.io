6c34eace90859cf22e95d31ea1f3f0bf7a5ea9a4
ImGuardia <156971861+ImGuardia@users.noreply.github.com>
2024-05-03 11:53:36 -0700

    more node stuff

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/creativesite'));
+app.use(express.static(__dirname + '/Main'));
 
 app.listen(1337, () => {
     console.log('Server started on http://localhost:1337');
