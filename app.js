bff41f20ba267e048a140e35e379aae5782c87b2
ImGuardia <156971861+ImGuardia@users.noreply.github.com>
2024-05-03 12:12:13 -0700

    node stuff v1.0.2

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use(express.static(__dirname + '/Main'));
-
-app.listen(1337, () => {
-    console.log('Server started on http://localhost:1337');
-    });
