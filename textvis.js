24c737f11fd45076d30790c5f9d497e43f7d8caf
jinheeh18 <jeneeh97818@gmail.com>
2024-05-06 15:15:47 -0700

    undo

--- textvis.js
+++ textvis.js
@@ -1,13 +1,13 @@
 
-// document.addEventListener("DOMContentLoaded", function () {
-//     // Delay the action by 10000ms (10 seconds)
-//     setTimeout(function () {
-//         // Display the div with class "bottomdiv"
-//         document.getElementById("myText").style.display = "block";
-//         // Set initial opacity to 0
-//         document.getElementById("myText").style.opacity = 0;
-//         // Animate the opacity to 1 over 1 second
-//         document.getElementById("myText").style.transition = "opacity 1s";
-//         document.getElementById("myText").style.opacity = 1;
-//     }, 3000); // 10 seconds
-// });
+document.addEventListener("DOMContentLoaded", function () {
+    // Delay the action by 10000ms (10 seconds)
+    setTimeout(function () {
+        // Display the div with class "bottomdiv"
+        document.getElementById("myText").style.display = "block";
+        // Set initial opacity to 0
+        document.getElementById("myText").style.opacity = 0;
+        // Animate the opacity to 1 over 1 second
+        document.getElementById("myText").style.transition = "opacity 1s";
+        document.getElementById("myText").style.opacity = 1;
+    }, 3000); // 10 seconds
+});
